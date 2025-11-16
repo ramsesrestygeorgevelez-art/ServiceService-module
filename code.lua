4bb70d77e84b0890f02fba60facac75d698be4aa
ramsesrestygeorgevelez-art <ramsesrestygeorgevelez@gmail.com>
2025-11-16 10:49:07 +0800

    Implement ServiceService module for service management
    This module implements a ServiceService system for creating and managing services with attributes and signals.

---
+++ code.lua
@@ -0,0 +1,56 @@
+-- ServiceService by RAMSES_SERVICESOWNER
+
+-- This module creates SPECIAL services! Plus it uses DRCS. (Don't Repeat Creating Services), a principle by me. And DRY. (Don't Repeat Yourself)
+
+--// ATTRIBUTES 
+
+-- CreateActorByService: Creates an "Actor" class when true.
+-- SetServiceType: Sets the type of the service. Can be loaded by MetaLoad.
+
+--// SERVICES / Modules
+
+local MetaLoad = require(script.Parent:WaitForChild("MetaLoad"))
+
+local ServiceService = {}
+
+function ServiceService:Create(name, funcTable, propTable, signals, parent)
+    
+    local self = setmetatable({}, ServiceService)
+    self.__load = MetaLoad:Load(self)
+    self.name = name
+    self.__funcs = funcTable
+    self.parent = parent
+    self.__properties = propTable
+    self.__actorCreateService = self:CreateProperty(self.name, "IsCreatedByActor", script:GetAttribute("CreateActorByService"))
+    self.__signals = signals
+    
+    return self
+end
+function ServiceService:GroupServices(tableOfServices)
+    local group = Instance.new("Folder")
+    local services = tableOfServices:unpack()
+    
+    services.parent = group
+end
+function ServiceService:CreateProperty(name, propertyName, value)
+    if name == self.name then
+        table.insert(self.__properties, [propertyName] = value)
+    end
+end
+function ServiceService:CreateSignal(signalName)
+   table.insert(self.__signals, signalName)
+end
+return ServiceService
+
+
+
+
+
+
+
+
+
+
+
+
+
